62878d7e1779c554ec0ef9d76fe9dc83bd3d6cc5
Doug Devine <11195375+hfuhruhurr@users.noreply.github.com>
2024-02-09 20:49:28 +0000

    all the fuckin changes dummy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Doug Devine",
+  title: "The Dude",
+  email: "thedude@gfy.com",
+  gitHub: "hfuhruhurr",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "dougdevine",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
